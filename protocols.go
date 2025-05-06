6f708e11958f09e6d34ca109044f40a06d1f3065
sherlock <wisdomnji@gmail.com>
2025-05-06 23:45:46 +0100

    update: so that's how (de)serialization works - interesting

--- protocols.go
+++ protocols.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"bytes"
 	"encoding/gob"
+	"encoding/binary"
 )
 
 type Headers struct {
 	Dh []byte
-	Pn int
-	N int
+	Pn uint32
+	N uint32
 }
 
 type States struct {
@@ -19,32 +21,56 @@ type States struct {
 	CKs []byte
 	CKr []byte
 
-	Ns int
-	Nr int
+	Ns uint32
+	Nr uint32
 
-	PN int
+	PN uint32
 
 	MKSKIPPED map[string]int
 }
 
-type Protocol interface {
-	Headers | States
+type ProtocolStates interface {
+	Serialize() (bytes.Buffer, error)
+	Deserialize(bytes.Buffer) error
 }
 
-func SerializeProtocols[V Protocol](protocol V) (bytes.Buffer, error) {
+type ProtocolsHeaders interface {
+	Serialize() ([]byte, error)
+	Deserialize([]byte) error
+}
+
+func (m States) Serialize() (bytes.Buffer, error) {
 	var buffer bytes.Buffer
 
 	enc := gob.NewEncoder(&buffer)
-	err := enc.Encode(protocol)
+	err := enc.Encode(m)
 
 	return buffer, err
 }
 
-func DeserializeProtocols[V Protocol](buffer bytes.Buffer) (V, error) {
+func (m *States) Deserialize(buffer bytes.Buffer) error {
 	dec := gob.NewDecoder(&buffer)
+	err := dec.Decode(&m)
+	return err
+}
 
-	var protocol V
-	err := dec.Decode(&protocol)
+func (m Headers) Serialize() ([]byte, error) {
+	result := make([]byte, (4*2) + len(m.Dh))
+	binary.LittleEndian.PutUint32(result[0:4], m.N)
+	binary.LittleEndian.PutUint32(result[4:8], m.Pn)
+	copy(result[8:], m.Dh)
+	return result, nil
+}
 
-	return protocol, err
+func (m *Headers) Deserialize(buffer []byte) error {
+	if len(buffer) < 8 {
+		return fmt.Errorf("buffer too short: need at least 8 bytes, got %d", len(buffer))
+	}
+
+	m.N = binary.LittleEndian.Uint32(buffer[0:4])
+	m.Pn = binary.LittleEndian.Uint32(buffer[4:8])
+	m.Dh = make([]byte, len(buffer)-8)
+	copy(m.Dh, buffer[8:])
+
+	return nil
 }
--- ratchets.go
+++ ratchets.go
@@ -3,7 +3,9 @@ package main
 import (
 	"io"
 	"crypto/sha512"
+	"crypto/sha256"
 	"golang.org/x/crypto/hkdf"
+	"crypto/hmac"
 )
 
 func AliceInit(
@@ -21,7 +23,20 @@ func AliceInit(
 	state.MKSKIPPED = make(map[string]int)
 }
 
-func BobInit() {
+func BobInit(
+	state *States,
+	SK []byte,
+	bobPrivateKey []byte,
+) {
+	state.DHs = bobPrivateKey
+        state.DHr = nil
+        state.RK = SK
+        state.CKs = nil
+        state.CKr = nil
+        state.Ns = 0
+        state.Nr = 0
+        state.PN = 0
+	state.MKSKIPPED = make(map[string]int)
 }
 
 func GENERATE_DH() []byte {
@@ -53,6 +68,40 @@ func DH(privKey []byte, pubKey []byte) []byte {
 	return KeypairAgree(privKey, pubKey)
 }
 
+func Encrypt(state *States, data []byte, AD []byte) (Headers, []byte) {
+	var mk = make([]byte, 0)
+	state.CKs, mk = KDF_CK(state.CKs)
+	header := Headers {
+		state.DHs,
+		state.PN,
+		state.Ns,
+	}
+	state.Ns += 1
+
+	// TODO:
+	// return header, ENCRYPT(mk, data, CONCAT(AD, header))
+	// return header, []byte{}
+	return header, mk
+}
+
+func KDF_CK(ck []byte) ([]byte, []byte) {
+	tck := hmac.New(sha256.New, ck)
+	CK := tck.Sum([]byte{0x01})
+	MK := tck.Sum([]byte{0x02})
+
+	return CK, MK
+}
+
+/*
+func CONCAT(ad []byte, header Headers) []byte {
+	serializedHeader := SerializeProtocols(header)
+	result := make([]byte, len(ad) + len(serializedHeader))
+	copy(result, ad)
+	copy(result[len(a):], serializedHeader)
+	return result
+}
+*/
+
 /*
 func DHRatchet(state States, header Headers) {
 	state.PN = state.Ns
@@ -64,17 +113,7 @@ func DHRatchet(state States, header Headers) {
 	state.RK, state.CKs = KDF_RK(state.Rk, DH(state.DHs, state.DHr))
 }
 
-
-
-func KDF_CK() []byte {
-}
-
-func Encrypt() []byte {
-}
-
 func Decrypt() []byte {
 }
 
-func CONCAT() []byte {
-}
 */
